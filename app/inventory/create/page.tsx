ee38a23eaf5db40b72e1508919afc6c7a36d95ff
Eric Kha <erkha102@gmail.com>
2025-12-08 22:32:30 -0800

    Added in try catch to handle the api call into the backend error

--- app/inventory/create/page.tsx
+++ app/inventory/create/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { Box, Button, Flex, Text, TextField } from '@radix-ui/themes';
-import React from 'react'
+import { Box, Button, Callout, Flex, Text, TextField } from '@radix-ui/themes';
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ValidationInventoryCreateItem } from '@/app/_components/invalidationInventoryCreate';
@@ -16,36 +16,54 @@ interface ItemForm {
 
 const CreateItem = () => {
 
+  const [isErrorApi, setIsErrorApi] = useState("")
   const { register, control, handleSubmit, formState: { errors } } = useForm<ItemForm>({
     resolver: zodResolver(ValidationInventoryCreateItem),
   });
 
 
-  const onSubmit = (values: any) => {
-    console.log("submit: ", values);
-    axios.post('/api/inventory', values);
-  }
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Flex direction="column" gap="3">
-        <Box maxWidth="250px">
-          <TextField.Root placeholder="Name of Item" {...register("name")} />
-          {errors.name && <Text color='red'>{errors.name.message}</Text>}
-        </Box>
-        <Box maxWidth="250px">
-          <TextField.Root placeholder="Description" {...register("description")} />
-          {errors.description && <Text color='red'>{errors.description.message}</Text>}
-        </Box>
-        <Box maxWidth="250px">
-          <TextField.Root type="number" placeholder="Quantity" {...register("quantity", {valueAsNumber: true })} />
-          {errors.quantity && <Text color='red'>{errors.quantity.message}</Text>}
-        </Box>
+  const onSubmit = async (values: ItemForm) => {
+    // console.log("submit: ", values);
+    // axios.post('/xapi/inventory', values);
+    // Added try catch for error handling
 
-        <Box>
-          <Button type="submit">Submit</Button>
-        </Box>
-      </Flex>
-    </form>
+    try {
+      await axios.post('/api/inventory', values);
+    } catch (error) {
+      setIsErrorApi("Error occurred while creating item.");
+    }
+  }
+
+  return (
+    <>
+      {isErrorApi && 
+        <Callout.Root my="2">
+        <Callout.Text color='red'>
+          {isErrorApi}
+        </Callout.Text>
+      </Callout.Root>
+      }
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Flex direction="column" gap="3">
+          <Box maxWidth="250px">
+            <TextField.Root placeholder="Name of Item" {...register("name")} />
+            {errors.name && <Text color='red'>{errors.name.message}</Text>}
+          </Box>
+          <Box maxWidth="250px">
+            <TextField.Root placeholder="Description" {...register("description")} />
+            {errors.description && <Text color='red'>{errors.description.message}</Text>}
+          </Box>
+          <Box maxWidth="250px">
+            <TextField.Root type="number" placeholder="Quantity" {...register("quantity", { valueAsNumber: true })} />
+            {errors.quantity && <Text color='red'>{errors.quantity.message}</Text>}
+          </Box>
+
+          <Box>
+            <Button type="submit">Submit</Button>
+          </Box>
+        </Flex>
+      </form>
+    </>
   )
 }
 
